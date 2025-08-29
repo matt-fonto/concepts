a5b5d3e5295f524197fd8ec0e904d371a6de3d16
matt-fonto <mateus.fonto@gmail.com>
2025-08-29 15:27:56 +0200

    data_structures: s. linked list -> append + prepend

---
+++ technical_interview/challenges/linkedLists/buildASinglyLinkedList.js
@@ -0,0 +1,68 @@
+// create a node
+
+// create a linked list
+// - append
+//  - prepend
+// - removeFirst
+// - removeLast
+// - print
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  // we don't pass anything here, only on the new node
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  //append
+  append(value) {
+    const newNode = new Node(value);
+
+    // if it's empty
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      // [10 -> 20 (tail)]
+      // append(30)
+      // 20.next = 30
+      // tail = 30
+      // 30.next = null (default constructor)
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+
+    this.length++;
+  }
+
+  // prepend
+  prepend(value) {
+    const newNode = new Node(value);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      // [(head) 10 -> 20 (tail)]
+      // prepend(30)
+      // 30.next => 10
+      // head => newNode
+
+      newNode.next = this.head; // point new node to old head
+      this.head = newNode; // point the head to the new node
+    }
+  }
+
+  // removeFirst
+  removeFirst() {}
+
+  // removeLast
+}
